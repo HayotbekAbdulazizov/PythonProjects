3d0fe91b76feac9dd8f1b0a0a4718ecac2bfb9dc
Hayotbek2005 <hayotbekabdulazizov2005@gmail.com>
2021-11-18 13:32:51 +1200

    transs
    1.0 added module to translate between latin and cyruillic transliterator.py
    1.1 added translator translates text withg google trabs but i think i was banned

---
+++ translator.py
@@ -0,0 +1,9 @@
+from googletrans import Translator
+translator = Translator()
+result = translator.translate('Hello World', src='en', dest='uz')
+
+print(result.src)
+print(result.dest)
+print(result.origin)
+print(result.text)
+print(result.pronunciation)
