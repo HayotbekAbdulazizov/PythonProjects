90f3857de639f94617a63bbf34df171064ec686d
Hayotbek2005 <hayotbekabdulazizov2005@gmail.com>
2021-11-19 13:26:01 +1200

    managing files

--- useful_algos.py
+++ useful_algos.py
@@ -35,3 +35,17 @@ def pdf_trans(url):
         pass
     except:
         pass
+
+
+
+
+
+
+# calculates sleep time
+time_ = str(input('Time >>>  '))
+hour = time_.split(':')[0]
+minute = time_.split(':')[1]
+time_ = int(hour) + 7
+if time_ > 24:
+    time_ -= 24
+print(f"{time_}:{minute}")
