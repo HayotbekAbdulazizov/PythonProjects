007ebf38cdf2964041956776d67321f37e935a69
HayotbekAbdulazizov <hayotbekabdulazizov2005@gmail.com>
2021-11-24 12:21:44 +1200

    update

--- request_test.py
+++ request_test.py
@@ -9,3 +9,22 @@ import requests
 
 url = 'http://localhost:8080/api/category/'
 r = requests.post(url, data={'status':400})
+
+request.get('instagram.com')
+
+
+
+
+
+# def schedule_api():
+#     r = requests.get("https://www.instagram.com/militsiya_102/")
+#     soup = BeautifulSoup(r.text, 'html.parser')
+#         # view = single_soup.find("div", {"class": "view"}).text
+#         # title = single_soup.find('div', {"class":"single-header__title"}).text	
+#         # body_code = single_soup.find('div', {"class":"single-content"})
+#     print(soup)
+#         # main_img_ls = body_code.findAll('img')
+
+
+
+# schedule_api()
