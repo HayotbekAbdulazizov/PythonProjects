789bee965965f074768abddc788baf3b3799ba16
HayotbekAbdulazizov <hayotbekabdulazizov2005@gmail.com>
2021-11-24 13:23:31 +1200

    del

--- geo.py
+++
@@ -1,7 +0,0 @@
-from geopy import GoogleV3
-
-place = "221 b Baker Street, London"
-location = GoogleV3().geocode(place)
-
-print(location.address)
-print(location.location)
--- openweather.py
+++
@@ -1,6 +0,0 @@
-import requests
-api_key = '54b9119af7f1b135da4edb201d08fdc6'
-r = requests.get("https://api.openweathermap.org/data/2.5/weather?q={london}&appid={54b9119af7f1b135da4edb201d08fdc6}")
-print(r)
-
-# api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
