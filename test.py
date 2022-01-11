c225ec0b2a947de709a5a35a52efdbe90d85ea13
Hayotbek Abdulazizov <hayotbekabdulazizov2005@gmail.com>
2022-01-12 01:47:36 +0600

    update test.py
    almost nothing

--- test.py
+++ test.py
@@ -59,19 +59,89 @@
     
 	
 
+# -*- coding: utf-8 -*-
+
+# from __future__ import unicode_literals
+
+# from djsms import send_text
+
+
+# frm = '+33123456789'
+# to = '+33987654321'
+# text = 'Please remember to pick up the bread before coming'
+# send_text(text, frm, to)
 
 
 
 
 
 
+# from textmagic.rest import TextmagicRestClient
+# username = "your_textmagic_username"
+# token = "your_apiv2_key"
+# client = TextmagicRestClient(username, token)
+# message = client.messages.create(phones="9990001001", text="Hello TextMagic")
+
+
+
+
+# from sms import send_sms
+
+# send_sms(
+#     'Here is the message',
+#     '+12065550100',
+#     ['+441134960000'],
+#     fail_silently=False
+# )
 
 
 
 
 
+# python script for sending message update
+
+import time
+from time import sleep
+from sinchsms import SinchSMS
+
+# function for sending SMS
+def sendSMS():
+
+	# enter all the details
+	# get app_key and app_secret by registering
+	# a app on sinchSMS
+	# number = 'your_mobile_number'
+	# number = '+447520650942'
+	number = '+998916020335'
+	app_key = '6b08ec28-fb1c-45ed-b832-d9351cb36fe6'
+	app_secret = 'RfLi4a1SE0ijcu5Bt8U5UA=='
+
+	# enter the message to be sent
+	message = 'Hello Message!!!'
+
+	client = SinchSMS(app_key, app_secret)
+	print("Sending '%s' to %s" % (message, number))
+
+	response = client.send_message(number, message)
+	message_id = response['messageId']
+	response = client.check_status(message_id)
+
+	# keep trying unless the status returned is Successful
+	while response['status'] != 'Successful':
+		print(response['status'])
+		time.sleep(1)
+		response = client.check_status(message_id)
+
+	print(response['status'])
+
+if __name__ == "__main__":
+	sendSMS()
 
 
+    # id 5b7f8d65ed874228a0070971d3904aa6
+    # api_token 7bfa5c19025c479f8abcc2d098a1a6c2
+    # url https://us.sms.api.sinch.com/xms/v1
+    # service plan id 5b7f8d65ed874228a0070971d3904aa6
 
 
 
@@ -87,3 +157,4 @@
 
     # KEY
     # SHA256:HbW3g8zUjNSksFbqTiUWPWg2Bq1x8xdGUrliXFzSnUw
+    # twlio secter xDW3YUB3_NwmTsKNB-qEiXGDZ4PLGmlBDraIFq4J
